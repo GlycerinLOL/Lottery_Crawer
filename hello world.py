9a4bb36114c77c2594eef1e0fe037b4fca8e4111
GlycerinLOL <brian.92308@gmail.com>
2019-12-19 00:45:39 +0800

    new

--- hello world.py
+++ hello world.py
@@ -1,2 +1,12 @@
-print("hello world")
-print('hello googel code in !')
+import requests
+from bs4 import BeautifulSoup
+url = "https://www.ptt.cc/bbs/MobileComm/index.html"
+for i in range(3):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text,"html.parser")
+    sel = soup.select('div.title a')
+    u = soup.select("div.btn-group.btn-group-paging a")
+    print('本頁的URL為'+url)
+    url = 'https://www.ptt.cc'+ u[1]['href']
+for s in sel:
+    print(s['href'],s.text)
