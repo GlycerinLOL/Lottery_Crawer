fd5a35f0f7dc3516d29b1c3acae9106617857650
GlycerinLOL <brian.92308@gmail.com>
2019-12-24 23:46:47 +0800

    g

--- lottery.py
+++ lottery.py
@@ -1,5 +1,5 @@
 import requests
-import csv
+import xlwings as xw
 from datetime import datetime
 from bs4 import BeautifulSoup
 
@@ -8,13 +8,19 @@ id1='Lotto649Control_history_dlQuery_SNo%s'
 id2='_0'
 r = requests.get(res) #get網頁資料
 index = BeautifulSoup(r.text,'html.parser') #將網頁資料轉成html.parser
-print('一般號碼:')
+num_list = []
 for i in range(1,7):
     lottery_num = i
     lottery_id = id1%(lottery_num)+id2
     normal = index.find('span',{'id':lottery_id})
     num = normal.text
-    print('',num) #抓出當期大樂透的六位中獎號
+    num_list.append(num)
+#print(num_list)    
 super_ = index.find('span',{'id':'SuperLotto638Control_history1_dlQuery_SNo7_0'})
 super_lottery = super_.text
-print('特別號:\n',super_lottery) #抓出當期特別號
+workbook = xw.Book('lottery.xlsx')
+sheet = workbook.sheets['工作表1']
+sheet.range('A1').current_region.end('down').row
+for n in range(1,7):
+    sheet.cells(1,n).value = num_list[n-1]
+workbook.save('lottery.xl')
