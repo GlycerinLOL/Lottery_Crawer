24b10522275f48b68e49696f075406b70f90b889
GlycerinLOL <brian.92308@gmail.com>
2019-12-25 00:16:11 +0800

    super

--- lottery.py
+++ lottery.py
@@ -18,9 +18,10 @@ for i in range(1,7):
 #print(num_list)    
 super_ = index.find('span',{'id':'SuperLotto638Control_history1_dlQuery_SNo7_0'})
 super_lottery = super_.text
+r_index = 'A'
 workbook = xw.Book('lottery.xlsx')
 sheet = workbook.sheets['工作表1']
-sheet.range('A1').current_region.end('down').row
-for n in range(1,7):
-    sheet.cells(1,n).value = num_list[n-1]
-workbook.save('lottery.xl')
+sheet.range('A1:F1').value = num_list
+type(sheet.range('A1:F1').current_region.end('down').row)
+
+
