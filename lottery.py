93f25f837df73165b8c59eac8eb3dad274e64c1c
GlycerinLOL <brian.92308@gmail.com>
2019-12-27 23:06:32 +0800

    finish data sort

--- lottery.py
+++ lottery.py
@@ -3,7 +3,6 @@ import json
 from datetime import datetime
 from bs4 import BeautifulSoup
 import sys,io,os
-sys.stdout=io.TextIOWrapper(sys.stdout.buffer,encoding='utf8')
 
 res = 'https://www.taiwanlottery.com.tw/Lotto/Lotto649/history.aspx'
 id1='Lotto649Control_history_dlQuery_SNo%s'
@@ -27,15 +26,18 @@ for i in range(1,7):
 super_ = index.find('span',{'id':'SuperLotto638Control_history1_dlQuery_SNo7_0'})
 super_lottery = super_.text #特別獎號碼
 #--------------------------------------------------------------------------------
+time_now = datetime.now()
+time_now = time_now.strftime("%m/%d/%Y, %H:%M:%S")
 data = {}
-data['latest_number'] = []
-data['latest_number'].append({
-    '開獎日期': time,
-    '獎金總額': total,
-    '普通獎': num_list,
-    '特別獎': super_lottery,
+data[time_now] = []
+data[time_now].append({
+    'Latest draw date': time,
+    'Total money': total,
+    'Normal prize': num_list,
+    'Special prize': super_lottery,
 })
-print(data)
+print(data) #大樂透資料存進dict
+#--------------------------------------------------------------------------------
 output_json = json.dumps(data, ensure_ascii=False)
 file_name = 'lottery_number.json'
 if os.path.isfile(file_name):
